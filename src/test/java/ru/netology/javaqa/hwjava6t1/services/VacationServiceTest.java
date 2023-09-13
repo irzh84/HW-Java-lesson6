92d6a4b5b25ad062dc4c29bf19cc04052ae56c57
Irina <izhts1482@gmail.com>
2023-09-13 15:16:36 +0300

    Initial commit

---
+++ src/test/java/ru/netology/javaqa/hwjava6t1/services/VacationServiceTest.java
@@ -0,0 +1,25 @@
+package ru.netology.javaqa.hwjava6t1.services;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class VacationServiceTest {
+    @Test
+    public void conditionFirst () {
+        VacationService service = new VacationService();
+        int expected = 3;
+        int actual = service.calculate(10_000, 3_000, 20_000);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void conditionSecond () {
+        VacationService service = new VacationService();
+        int expected = 2;
+        int actual = service.calculate(100_000, 60_000, 150_000);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+}
